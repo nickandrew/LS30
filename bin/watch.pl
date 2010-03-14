b0c3b4d4eac0702481d5561909eb58912395226c
Nick Andrew <nick@nick-andrew.net>
2010-03-15 09:37:48 +1100

    Add bin/watch.pl script
    This script runs a class (specified on the command line) which loops
    endlessly, processing messages from the device. The default class
    is 'Watch' which decodes and logs all messages. This Watch class can
    be subclassed to implement different behaviour.

---
+++ bin/watch.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl -w
+#   vim:sw=4:ts=4:
+#   Copyright (C) 2010, Nick Andrew <nick@nick-andrew.net>
+#   Licensed under the terms of the GNU General Public License, Version 3
+#
+#  Watch the messages and react to them
+#
+#  Usage: watch2.pl [options]
+#
+#  Options:
+#    -c classname        Watching class to use (default is 'Watch')
+#    -h host:port        Address of LS30 device or server
+
+use Data::Dumper qw(Dumper);
+use Getopt::Std qw(getopts);
+
+use Selector qw();
+
+use vars qw($opt_c $opt_h);
+
+$| = 1;
+getopts('c:h:');
+
+my $class = $opt_c || 'Watch';
+
+eval "require $class";
+if ($@) {
+	die "Unable to require class $class";
+}
+
+my $selector = Selector->new();
+my $watcher = $class->new($opt_h);
+
+$watcher->addSelector($selector);
+
+$selector->eventLoop();
+
+exit(0);
