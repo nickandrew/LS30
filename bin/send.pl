d3656e9f7015e4b60c8a30a61f9116f9165577ea
Nick Andrew <nick@nick-andrew.net>
2010-03-28 13:52:36 +1100

    Use LS30::Commander & parse response
    Good to see how good the response parsing is.

--- bin/send.pl
+++ bin/send.pl
@@ -8,6 +8,8 @@
 use Data::Dumper qw(Dumper);
 use Getopt::Std qw(getopts);
 
+use LS30::Commander qw();
+use LS30::ResponseMessage qw();
 use LS30Command qw();
 use LS30Connection qw();
 
@@ -21,10 +23,12 @@ $ls30c->Connect();
 
 LS30Command::addCommands();
 
+my $ls30cmdr = LS30::Commander->new($ls30c);
+
 my @responses;
 
 foreach my $cmd (@ARGV) {
-	my $response = $ls30c->sendCommand($cmd);
+	my $response = $ls30cmdr->sendCommand($cmd);
 
 	push(@responses, [ $cmd, $response ]);
 }
@@ -33,6 +37,8 @@ foreach my $lr (@responses) {
 	my ($cmd, $response) = @$lr;
 
 	printf("%-40s | %s\n", $cmd, $response);
+	my $resp = LS30::ResponseMessage->new($response);
+	print Dumper($resp) if ($resp);
 }
 
 exit(0);
