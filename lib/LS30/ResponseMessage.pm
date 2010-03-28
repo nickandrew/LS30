a6f90d5c78df6d343eba808790a3547d008c4abf
Nick Andrew <nick@nick-andrew.net>
2010-03-28 13:51:55 +1100

    Add LS30::ResponseMessage
    This is a first cut at a response parser - but actually, it just
    uses the parsing functions of LS30Command.

---
+++ lib/LS30/ResponseMessage.pm
@@ -0,0 +1,79 @@
+#!/usr/bin/perl -w
+#   vim:sw=4:ts=4:
+#   Copyright (C) 2010, Nick Andrew <nick@nick-andrew.net>
+#   Licensed under the terms of the GNU General Public License, Version 3
+
+=head1 NAME
+
+LS30::ResponseMessage - Parsed response message
+
+=head1 DESCRIPTION
+
+This class parses response messages from the LS30 (e.g. "!a0&").
+
+=head1 METHODS
+
+=over
+
+=cut
+
+package LS30::ResponseMessage;
+
+use strict;
+
+use Carp qw(confess);
+
+use LS30Command qw();
+
+# ---------------------------------------------------------------------------
+
+=item new($string)
+
+Parse $string and return a new LS30::ResponseMessage.
+
+If string is not supplied, return an empty object.
+
+=cut
+
+sub new {
+	my ($class, $string) = @_;
+
+	my $self = { };
+	bless $self, $class;
+
+	if ($string) {
+		$self->_parseString($string);
+	}
+
+	return $self;
+}
+
+
+# ---------------------------------------------------------------------------
+
+=item _parseString($string)
+
+Parse the string and update $self.
+
+=cut
+
+sub _parseString {
+	my ($self, $string) = @_;
+
+	if ($string !~ /^!(.+)&$/) {
+		# Doesn't look like a response
+		confess "Invalid ResponseMessage string: $string";
+	}
+
+	my $return = LS30Command::parseResponse($string);
+	if (! $return) {
+		$self->{'error'} = "Unparseable response $string";
+		return;
+	}
+
+	foreach my $k (keys %$return) {
+		$self->{$k} = $return->{$k};
+	}
+}
+
+1;
