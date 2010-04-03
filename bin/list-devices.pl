16b1e7e5d74ec45415f1c6b64582c74a60b37afb
Nick Andrew <nick@nick-andrew.net>
2010-04-03 12:46:13 +1100

    Use LS30::Type
      - for a list of commands to construct
      - to help format the parsed response

--- bin/list-devices.pl
+++ bin/list-devices.pl
@@ -9,6 +9,7 @@ use Data::Dumper qw(Dumper);
 use Getopt::Std qw(getopts);
 
 use LS30::Commander qw();
+use LS30::Type qw();
 use LS30Command qw();
 use LS30Connection qw();
 
@@ -25,20 +26,13 @@ LS30Command::addCommands();
 my $ls30cmdr = LS30::Commander->new($ls30c);
 
 my @responses;
-
-my $types = {
-	'Burglar Sensor' => 'b',
-	'Controller' => 'c',
-	'Fire Sensor' => 'f',
-	'Medical Button' => 'm',
-	'Extra Sensor' => 'e',
-};
+my @device_code_list = LS30::Type::listStrings('Device Code');
 
 my $s = '';
 my $s2 = '';
 
-foreach my $type (sort (keys %$types)) {
-	my $code = $types->{$type};
+foreach my $device_name (@device_code_list) {
+	my $code = LS30::Type::getCode('Device Code', $device_name);
 
 	foreach my $device_number qw(00 01 02 03 04 05 06 07 08 09) {
 		my $cmd = sprintf("!k%s?%2s&", $code, $device_number);
@@ -50,10 +44,13 @@ foreach my $type (sort (keys %$types)) {
 		}
 
 		if ($response =~ /!k.(..)(......)(....)(..)(..)(..)(........)(.+)&/) {
-			my ($junk1, $dev_id, $junk2, $junk3, $z, $c, $config, $rest) = ($1, $2, $3, $4, $5, $6, $7, $8);
+			my ($dev_type, $dev_id, $junk2, $junk3, $z, $c, $config, $rest) = ($1, $2, $3, $4, $5, $6, $7, $8);
 
-			$s .= sprintf("%s %s-%s ID is %s\n",
-				$type,
+			my $dev_type_string = LS30::Type::getString('Device Specific Type', $dev_type);
+
+			$s .= sprintf("%s %s %s-%s ID is %s\n",
+				$device_name,
+				$dev_type_string,
 				$z,
 				$c,
 				$dev_id,
